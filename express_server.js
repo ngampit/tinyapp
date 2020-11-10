65243bfdb492ee67c040f19e0b1a4c73a6144669
ngampit <ngampitt@yahoo.com>
2020-11-10 13:27:42 -0800

    install nodeman, set default npm test to nodeman directory

--- express_server.js
+++ express_server.js
@@ -2,13 +2,19 @@ const express = require("express");
 const app = express();
 const PORT = 3000; // default port 8080
 
+
 app.set("view engine", "ejs");
+
+
+
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
 app.get('/', (req, res) => {
   res.send("Hello!");
 });
@@ -38,10 +44,36 @@ app.get('/urls', (req,res)=>{
   res.render("urls_index", templateVars);
 })
 
+app.get('/urls/new', (req,res)=>{
+    res.render('urls_new');
+})
+
 app.get("/urls/:shortURL", (req, res) => {
     const templateVars = { shortURL: req.params.shortURL, longURL: "http://www.lighthouselabs.ca" };
     res.render("urls_show", templateVars);
+});
+
+
+
+app.post("/urls", (req, res) => {
+    //console.log(req.body); // Log the POST request body to the console
+    
+    const longUrlContainer = req.body.longURL;
+    const shortUrlContainer = generateRandomString(6);
+ 
+    urlDatabase[shortUrlContainer] = longUrlContainer;
+        
+    res.send("Ok");         // Respond with 'Ok' (we will replace this)
   });
+
+function generateRandomString(length) {
+    const randomChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let result = '';
+    for (let i = 0; i < length; i++ ) {
+        result += randomChars.charAt(Math.floor(Math.random() * randomChars.length));
+    }
+    return result;
+}  
 
 });
 
