64f4f12f5e40f63c839de42fbca197f515391c12
ngampit <ngampitt@yahoo.com>
2020-11-09 17:59:25 -0800

    add shortURL edited

--- express_server.js
+++ express_server.js
@@ -24,19 +24,24 @@ app.get('/hello', (req,res)=>{
   res.send('<html><body>Hello <b>World</b></body></html>\n');
 })
 
-app.get("/set", (req, res) => {
- const a = 1;
- res.send(`a = ${a}`);
-});
+// app.get("/set", (req, res) => {
+//  const a = 1;
+//  res.send(`a = ${a}`);
+// });
 
-app.get("/fetch", (req, res) => {
- res.send(`a = ${a}`);
-});
+// app.get("/fetch", (req, res) => {
+//  res.send(`a = ${a}`);
+// });
 
 app.get('/urls', (req,res)=>{
   const templateVars = { urls: urlDatabase }; 
   res.render("urls_index", templateVars);
 })
+
+app.get("/urls/:shortURL", (req, res) => {
+    const templateVars = { shortURL: req.params.shortURL, longURL: "http://www.lighthouselabs.ca" };
+    res.render("urls_show", templateVars);
+  });
 
 });
 
